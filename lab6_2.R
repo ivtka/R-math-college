41a74b8b530f3624f5a651ab78a6d89230d8c542
Ivan Tkachuk <ivtkac@gmail.com>
2022-04-01 15:08:56 +0300

    added lab 10

--- lab6_2.R
+++ lab6_2.R
@@ -1,10 +1,12 @@
 f <- function(x) {
-  return (4^x*(7.5-x^2)^(1/3))
+  return(4^x * (7.5 - x^2)^(1 / 3))
 }
 
 x <- 2.5
 h <- 0.001
 
-df <- c((f(x + h) - f(x)) / h,
-        (f(x + h) - f(x - h)) / (2 * h),
-        (f(x) - f(x - h)) / h)
+df <- c(
+  (f(x + h) - f(x)) / h,
+  (f(x + h) - f(x - h)) / (2 * h),
+  (f(x) - f(x - h)) / h
+)
